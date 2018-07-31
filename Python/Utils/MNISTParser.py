9c1a71eefaf935ad63b3b2893a39c7e7e5f50c80
Edmund Lam <edlam1889@gmail.com>
2018-07-30 18:21:10 -0700

    fix parsing, sigmoid

--- Python/Utils/MNISTParser.py
+++ Python/Utils/MNISTParser.py
@@ -12,9 +12,8 @@ MNISTDATA = {"test":["t10k-images-idx3-ubyte.gz", "t10k-labels-idx1-ubyte.gz"], 
 
 def parse_data(s="train"):
     with gzip.open(os.path.join(path, MNISTDATA[s][1]), 'rb') as f:
-        labels = array("B", f.read())
-    # print(labels)
+        labels = array("B", f.read())[8:]
     with gzip.open(os.path.join(path, MNISTDATA[s][0]), 'rb') as f:
-        images = map(lambda x:x.tolist(), np.array_split(array("B", f.read()), len(labels)))
+        images = map(lambda x:x.tolist(), np.array_split(array("B", f.read())[16:], len(labels)))
 
     return list(zip(images, labels))
--- Python/Utils/MathUtils.py
+++ Python/Utils/MathUtils.py
@@ -2,7 +2,10 @@ import numpy as np
 
 
 def sigmoid(x):
-    return 1.0/(1.0+np.exp(-x))
+    return .5 * (1 + np.tanh(.5 * x))
+    # try:
+    #     return 1.0/(1.0+np.exp(-x))
+    # except RuntimeWarning:
 
 def sigmoid_derivative(x):
     return x*(1-x);
